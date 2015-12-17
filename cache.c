d5de197de2313bf4b4ef4a9294716d1a3248bbf0
22a <meehanp2@tcd.ie>
2015-12-17 21:07:26 +0000

    git is a thing :ocean:

---
+++ cache.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct cacheEntry;
+
+typedef struct cacheEntry {
+  struct cacheEntry *next;
+  struct cacheEntry *prev;
+  int *address;
+} cacheEntry;
+
+typedef struct lruCache {
+  int maxCount;
+  cacheEntry *first;
+  cacheEntry *last;
+} lruCache;
+
+bool inCache(char* addr, lruCache cache){
+  int i;
+  int address = (int)strtol(addr, NULL, 16);
+  cacheEntry* current = cache.first;
+  cacheEntry* scratch;
+
+  if (current == NULL){
+    // will occur on first cache access attempt
+    current = malloc(sizeof(cacheEntry));
+    *current->address = address;
+    return false;
+  }
+
+  for(i=0; i < (cache.maxCount); i++){
+    if(*current->address == address){
+      return true;
+      //move current* to head of list
+    }
+    else {
+      if(current->next == NULL && i < cache.maxCount-1){
+        // if we're not at at the end of the cache and the next element in the list is null
+        // make a new cache entry and put it at the start of the list
+        scratch = malloc(sizeof(cacheEntry));
+        *scratch->address = address;
+        scratch->next = cache.first;
+        cache.first->prev = scratch;
+        cache.first = scratch;
+        // scoot back up on the list
+        cache.last = cache.last->prev;
+        if (cache.last != NULL){
+          // if the something was ejected from the lru, free the memory and discard pointer to it
+          free(cache.last->next);
+          cache.last->next = NULL;
+        }
+      }
+      else {
+        // we didn't match, move on to next element in cache
+        current = current->next;
+      }
+    }
+  }
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  printf("hello world\n");
+
+
+  return 0;
+
+}
